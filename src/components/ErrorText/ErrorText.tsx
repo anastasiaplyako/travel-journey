aec734a2909cf657bba1a70684d0497c4e8837fc
anastasiaplyako <anastasiareiz@gmail.com>
2023-09-16 03:24:34 +0400

    Updates

--- src/components/ErrorText/ErrorText.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import styles from './ErrorText.module.scss';
-
-const ErrorText = ({ error }: { error: string }) => (<div className={styles.errorText}>{error}</div>)
-
-export default ErrorText;
